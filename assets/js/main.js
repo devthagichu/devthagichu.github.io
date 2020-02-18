8b40162592817b0d00011eccf92de8f23fe191bd
Thagichu Anthony <anthonythagichu@gmail.com>
2020-02-18 14:12:36 +0300

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+const hero__text = document.getElementById('hero__text');
+const menu__logo = document.getElementById('menu__logo');
+
+
+
+
+window.addEventListener('scroll', function (event) {
+    var scroll = this.scrollY;
+
+    if (scroll > 20) {
+        menu__logo.classList.add("logo__icon");
+        menu__logo.classList.remove("hidden");
+
+    } else {
+        menu__logo.classList.add("hidden");
+        menu__logo.classList.remove("logo__icon");
+    }
+
+    var header = document.getElementById("menu");
+    var sticky = header.offsetTop;
+
+
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+
+
+    } else {
+        header.classList.remove("sticky");
+    }
+
+
+    hero__text.style.cssText = "transform: translate(0px , " + scroll / 2 + "%)";
+
+
+
+});
+
+
+
+
+
